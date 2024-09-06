52535d6d4eb361de74c6973ddc5ac85cb81356eb
Jake Bailey <jake@jaketbailey.co.uk>
2024-09-06 16:12:54 +0100

    01_05

---
+++ Rust-Essentials/01_05_building_programs_using_cargo/begin/hello_world/target/release/deps/hello_world-24ffd6020ede8d9a.d
@@ -0,0 +1,5 @@
+/home/jaketbailey/git/learning-rust/Rust-Essentials/01_05_building_programs_using_cargo/begin/hello_world/target/release/deps/hello_world-24ffd6020ede8d9a: src/main.rs
+
+/home/jaketbailey/git/learning-rust/Rust-Essentials/01_05_building_programs_using_cargo/begin/hello_world/target/release/deps/hello_world-24ffd6020ede8d9a.d: src/main.rs
+
+src/main.rs:
---
+++ Rust-Essentials/01_05_building_programs_using_cargo/begin/hello_world/target/release/hello_world.d
@@ -0,0 +1,1 @@
+/home/jaketbailey/git/learning-rust/Rust-Essentials/01_05_building_programs_using_cargo/begin/hello_world/target/release/hello_world: /home/jaketbailey/git/learning-rust/Rust-Essentials/01_05_building_programs_using_cargo/begin/hello_world/src/main.rs
